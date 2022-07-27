0d2e920cf108dc377f08ffeff1dcff15152586ad
esadakman <esadd26@gmail.com>
2022-07-27 14:30:31 +0300

    home finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Body, LinesWrapper, Paragraph, SkewLine } from "./styles/Home.styled";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Body>
+        <LinesWrapper>
+          <SkewLine>
+            <Paragraph></Paragraph>
+            <Paragraph>Lorem</Paragraph>
+          </SkewLine>
+          <SkewLine>
+            <Paragraph>Lorem</Paragraph>
+            <Paragraph>IPSUM</Paragraph>
+          </SkewLine>
+          <SkewLine>
+            <Paragraph>IPSUM</Paragraph>
+            <Paragraph>DOLOR</Paragraph>
+          </SkewLine>
+          <SkewLine>
+            <Paragraph>DOLOR</Paragraph>
+            <Paragraph>SIT AMET</Paragraph>
+          </SkewLine>
+          <SkewLine>
+            <Paragraph>SIT AMET</Paragraph>
+
+            <Paragraph></Paragraph>
+          </SkewLine>
+        </LinesWrapper>
+      </Body>
+    </div>
+  );
 };
 
 export default Home;
