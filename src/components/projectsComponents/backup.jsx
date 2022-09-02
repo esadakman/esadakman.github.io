e3975546be5b59b4922cdf5979030c29235bcfbd
esadakman <esadd26@gmail.com>
2022-09-02 09:29:29 +0300

    reactProjects

---
+++ src/components/projectsComponents/backup.jsx
@@ -0,0 +1,388 @@
+// {/* <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/FireBlog-Milestone")
+// }
+// >
+// <Card>
+//   <img src={fireBlog} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project, I designed a Fire Blog with many features.
+//       </li>
+//       <li>
+//         I used context API for state management, Firebase for
+//         authentication, and Firestore database for CRUD operations.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Fire Blog </p>
+// </TitleCard>
+// </CardWrapper>
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/firecontact-app")
+// }
+// >
+// <Card>
+//   <img src={fireContact} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project, I designed a Contact App for understanding
+//         CRUD operations.
+//       </li>
+//       <li>I used Firebase Realtime Database for database.</li>
+//       <li>
+//         User can update the list by entering name, phone number and
+//         gender information.And also user can delete or fix any contact
+//         from table.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p> FireContact App</p>
+// </TitleCard>
+// </CardWrapper>
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/movie-app-firebase")
+// }
+// >
+// <Card>
+//   <img src={MovieApp} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     {/* <p> */}
+//     <ul>
+//       <li>
+//         In this project, I designed a Movie App with many features. I
+//         provided the movie information with the TMDB API.
+//       </li>
+//       <li>I used Firebase for authentication.</li>
+//       <li>
+//         The user can log in and register with both their email address
+//         and their Google account.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p> Movie App</p>
+// </TitleCard>
+// </CardWrapper>
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/recipe-app")
+// }
+// >
+// <Card>
+//   <img src={RecipeApp} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>In this project i tried to make a recipe app.</li>
+//       <li>I used local storage for authentication.</li>
+//       <li>
+//         After login you can search food's recipe by their name and
+//         type.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Recipe App</p>
+// </TitleCard>
+// </CardWrapper>
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/task-tracker-react")
+// }
+// >
+// <Card>
+//   <img src={taskTracker} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project i tried to make a build simple task tracker
+//         app.
+//       </li>
+//       <li>
+//         User can add new task to tracker list with date and time
+//       </li>
+//       <li>And remove them from list after finish the task.</li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Task Tracker App</p>
+// </TitleCard>
+// </CardWrapper>
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/api-tutorial-project")
+// }
+// >
+// <Card>
+//   <img src={apiTutorial} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project, I made CRUD operations using the real API.{" "}
+//       </li>
+//       <li>You can add, delete or edit new entries in the list.</li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>API Tutorial</p>
+// </TitleCard>
+// </CardWrapper>
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/random-user-app-2 ")
+// }
+// >
+// <Card>
+//   <img src={randomUserTwo} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project i make random user app, but this time you can
+//         save the users info's and when you hover the logos in the menu
+//       </li>
+//       <li>User's information will change depending on the logo.</li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Random User App 2</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/react-weather-app ")
+// }
+// >
+// <Card>
+//   <img src={weatherApp} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>Remake of the javaScript Weather App with React</li>
+//       <li>
+//         Project aims to display current weather data of searched city
+//         around the world with fetching Weather Api data.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Weather App</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/axios-router-project ")
+// }
+// >
+// <Card>
+//   <img src={axiosRouterExercise} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         I prepared a small-scale project in order to understand the
+//         Router Dom and the Material UI framework that I have learned
+//         recently.
+//       </li>
+//       <li>
+//         In addition to Material UI, I also used the Bootstrap for
+//         pagination process and card design.
+//       </li>
+//       <li></li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Axios-Router Project</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/random-user-react")
+// }
+// >
+// <Card>
+//   <img src={randomUserOne} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project i tried to make a simple random user app.
+//       </li>
+//       <li>I use axios for fetching data. </li>
+//       <li>User can get random profiles by clicking the button</li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Random User App</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab(
+//     " https://github.com/esadakman/reviews-project/blob/master/README.md"
+//   )
+// }
+// >
+// <Card>
+//   <img src={reviewsProject} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>Project aims to create a Review Project App.</li>
+//       <li>
+//         Customers comments change automatically, and they can also be
+//         changed by clicking the go and back buttons
+//       </li>
+//       <li>
+//         If the user wants to see a random comment, he/she can press
+//         the "Surprise Me" button and see it. Customer informations
+//         comes from local json file.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Reviews Project</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab(" https://github.com/esadakman/stopWatch-react")
+// }
+// >
+// <Card>
+//   <img src={stopWatch} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         For understanding useState hooks and components i made a
+//         simple StopWatch app.{" "}
+//       </li>
+//       <li>
+//         It has only three features which are play, pause and reset.
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>StopWatch</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/react-digital-clock ")
+// }
+// >
+// <Card>
+//   <img src={digitalClock} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         Remake of the javaScript digital clock project with React
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Digital Clock</p>
+// </TitleCard>
+// </CardWrapper>
+// {/* //! ============Divider=========================== */}
+
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/language-cards-react ")
+// }
+// >
+// <Card>
+//   <img src={languageCards} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project for understanding react structure, i tried to make a simple language cards app.
+//       </li>
+//       <li>
+//         {" "}
+//         I get the data from locally and i use SASS for the design
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Language Cards</p>
+// </TitleCard>
+// </CardWrapper>
+
+// {/* //! ============Divider=========================== */}
+// <CardWrapper
+// title="Click for details"
+// onClick={() =>
+//   openInNewTab("https://github.com/esadakman/tour-places-react")
+// }
+// >
+// <Card>
+//   <img src={TourPlaces} alt="poster" />
+//   <Desc>
+//     <h2>Overview</h2>
+//     <ul>
+//       <li>
+//         In this project for understanding react structure, i tried to
+//         make a simple tour places app.{" "}
+//       </li>
+//       <li>
+//         I get the data from locally and i use SASS for the design
+//       </li>
+//     </ul>
+//   </Desc>
+// </Card>
+// <TitleCard>
+//   <p>Tour Places</p>
+// </TitleCard>
+// </CardWrapper>
+// //! ============Divider=========================== */}
