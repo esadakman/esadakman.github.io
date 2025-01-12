7eb9a274b5995597d8af6e26813309e2c3c77aba
esadakman <esadd26@gmail.com>
2025-01-12 11:22:30 +0300

    deploy fix

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "../components/Footer";
 import { GlobalStyles } from "../components/globalStyles/Global.styled";
 import Navbar from "../components/Navbar";
-import HtmlCssProjects from "../components/projectsComponents/HtmlCssProjects";
-import JavaScript from "../components/projectsComponents/JavaScriptProjects";
-import ReactProjects from "../components/projectsComponents/ReactProjects";
+// import HtmlCssProjects from "../components/projectsComponents/HtmlCssProjects";
+// import JavaScript from "../components/projectsComponents/JavaScriptProjects";
+// import ReactProjects from "../components/projectsComponents/ReactProjects";
 import ScrolToTop from "../pages/ScrolToTop";
 import Home from "../pages/Home";
-import DjangoProjects from "../components/projectsComponents/DjangoProjects";
+// import DjangoProjects from "../components/projectsComponents/DjangoProjects";
 // import Projects from "../pages/Projects";
 
 const Router = () => {
@@ -21,10 +21,10 @@ const Router = () => {
         <Routes>
           <Route path="/" exact element={<Home />} />
           {/* <Route path="/projects" element={<ReactProjects />} /> */}
-          <Route path="/react" exact element={<ReactProjects />} />
+          {/* <Route path="/react" exact element={<ReactProjects />} />
           <Route path="/javaScript" exact element={<JavaScript />} />
           <Route path="/htmlCss" exact element={<HtmlCssProjects />} />
-          <Route path="/django" exact element={<DjangoProjects />} />
+          <Route path="/django" exact element={<DjangoProjects />} /> */}
         </Routes>
         <Footer />
       </BrowserRouter>
