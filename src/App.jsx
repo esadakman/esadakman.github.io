463125a494462eecbd34f644e9af7ceac4e4c01c
esadakman <esadd26@gmail.com>
2026-06-21 15:30:00 +0300

    redesign init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { ThemeProvider } from "styled-components";
-import theme from "./components/globalStyles/theme";
+import { ThemeModeProvider } from "./components/globalStyles/ThemeContext";
 import Router from "./router/Router";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <ToastContainer position="top-right" />
+    <ThemeModeProvider>
+      <ToastContainer position="top-right" theme="colored" />
       <Router />
-    </ThemeProvider>
+    </ThemeModeProvider>
   );
 }
 
