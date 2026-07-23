1f695dd4601664a0e494c52eec07c928ea394b37
esadakman <esadd26@gmail.com>
2026-07-23 10:56:28 +0300

    contact spacing updates

--- src/components/componentsStyles/Contact.styled.jsx
+++ src/components/componentsStyles/Contact.styled.jsx
@@ -6,7 +6,7 @@ export const ContactContainer = styled.div`
   align-items: center;
   width: 100%;
   min-height: calc(100vh - 7.5rem);
-  padding: 3rem 0;
+  padding-bottom: 1rem;
   background-color: ${({ theme }) => theme.colors.heroBg};
   background-image: radial-gradient(
       800px 400px at 20% 0%,
@@ -142,7 +142,7 @@ export const StyledContactForm = styled.div`
     width: 22rem;
     text-align: center;
   }
-  @media (max-width: ${({ theme }) => theme.size.md}) { 
+  @media (max-width: ${({ theme }) => theme.size.md}) {
     flex: 1;
   }
 
